920b2da69abaf336abb605c79306e1869a4295e0
Deepak Kumar <deepakkumar@Deepaks-MacBook-Pro.local>
2025-11-07 07:32:44 +0500

    test

--- src/app/home/HomePage.js
+++ src/app/home/HomePage.js
@@ -2,7 +2,7 @@ import HeroSection from "./components/HeroSection";
 import WelcomeSection from "./components/WelcomeSection";
 // import MidBanner from "./components/MidBanner";
 import FormSection from "./components/FormSection";
-// import CardsSection from "./components/CardsSection";
+import CardsSection from "./components/CardsSection";
 // import CompaniesSlider from "@/components/common/CompaniesSlider";
 // import TeamContent from "./components/TeamSection";
 
@@ -14,7 +14,7 @@ export default function HomePage()
             <WelcomeSection />
             {/* <MidBanner /> */}
             <FormSection />
-            {/* <CardsSection /> */}
+            <CardsSection />
             {/* <TeamContent /> */}
             {/* <CompaniesSlider /> */}
         </div>
